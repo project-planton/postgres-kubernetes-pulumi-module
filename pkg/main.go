4c4354d398b0637a40d8f064305936cabf696f49
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-14 16:00:00 +0530

    simplified further

--- pkg/main.go
+++ pkg/main.go
@@ -2,9 +2,11 @@ package pkg
 
 import (
 	"github.com/pkg/errors"
+	zalandov1 "github.com/plantoncloud/kubernetes-crd-pulumi-types/pkg/zalandooperator/acid/v1"
 	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/kubernetes/postgreskubernetes"
 	"github.com/plantoncloud/pulumi-module-golang-commons/pkg/provider/kubernetes/pulumikubernetesprovider"
 	kubernetescorev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	kubernetesmetav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -16,6 +18,7 @@ type ResourceStack struct {
 
 func (s *ResourceStack) Resources(ctx *pulumi.Context) error {
 	locals := initializeLocals(ctx, s.Input)
+
 	//create kubernetes-provider from the credential in the stack-input
 	kubernetesProvider, err := pulumikubernetesprovider.GetWithKubernetesClusterCredential(ctx,
 		s.Input.KubernetesClusterCredential, "kubernetes")
@@ -24,29 +27,129 @@ func (s *ResourceStack) Resources(ctx *pulumi.Context) error {
 	}
 
 	//create namespace resource
-	createdNamespace, err := kubernetescorev1.NewNamespace(ctx, locals.Namespace, &kubernetescorev1.NamespaceArgs{
-		Metadata: metav1.ObjectMetaPtrInput(&metav1.ObjectMetaArgs{
-			Name:   pulumi.String(locals.Namespace),
-			Labels: pulumi.ToStringMap(s.Labels),
-		}),
-	}, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "5s", Update: "5s", Delete: "5s"}),
+	createdNamespace, err := kubernetescorev1.NewNamespace(ctx,
+		locals.Namespace,
+		&kubernetescorev1.NamespaceArgs{
+			Metadata: metav1.ObjectMetaPtrInput(&metav1.ObjectMetaArgs{
+				Name:   pulumi.String(locals.Namespace),
+				Labels: pulumi.ToStringMap(s.Labels),
+			}),
+		}, pulumi.Timeouts(&pulumi.CustomTimeouts{Create: "5s", Update: "5s", Delete: "5s"}),
 		pulumi.Provider(kubernetesProvider))
 	if err != nil {
 		return errors.Wrapf(err, "failed to create %s namespace", locals.Namespace)
 	}
 
 	//create zalando postgresql resource
-	if err := postgresql(ctx, locals, createdNamespace, s.Labels); err != nil {
-		return errors.Wrap(err, "failed to create postgresql resource")
+	_, err = zalandov1.NewPostgresql(ctx,
+		"database",
+		&zalandov1.PostgresqlArgs{
+			Metadata: metav1.ObjectMetaArgs{
+				// for zolando operator the name is required to be always prefixed by teamId
+				// a kubernetes service with the same name is created by the operator
+				Name:      pulumi.Sprintf("%s-%s", vars.TeamId, locals.PostgresKubernetes.Metadata.Id),
+				Namespace: createdNamespace.Metadata.Name(),
+				Labels:    pulumi.ToStringMap(s.Labels),
+			},
+			Spec: zalandov1.PostgresqlSpecArgs{
+				NumberOfInstances: pulumi.Int(locals.PostgresKubernetes.Spec.Container.Replicas),
+				Patroni:           zalandov1.PostgresqlSpecPatroniArgs{},
+				PodAnnotations: pulumi.ToStringMap(map[string]string{
+					"postgres-cluster-id": locals.PostgresKubernetes.Metadata.Id,
+				}),
+				Postgresql: zalandov1.PostgresqlSpecPostgresqlArgs{
+					Version: pulumi.String(vars.PostgresVersion),
+					Parameters: pulumi.StringMap{
+						"max_connections": pulumi.String("200"),
+					},
+				},
+				Resources: zalandov1.PostgresqlSpecResourcesArgs{
+					Limits: zalandov1.PostgresqlSpecResourcesLimitsArgs{
+						Cpu:    pulumi.String(locals.PostgresKubernetes.Spec.Container.Resources.Limits.Cpu),
+						Memory: pulumi.String(locals.PostgresKubernetes.Spec.Container.Resources.Limits.Memory),
+					},
+					Requests: zalandov1.PostgresqlSpecResourcesRequestsArgs{
+						Cpu:    pulumi.String(locals.PostgresKubernetes.Spec.Container.Resources.Requests.Cpu),
+						Memory: pulumi.String(locals.PostgresKubernetes.Spec.Container.Resources.Requests.Memory),
+					},
+				},
+				TeamId: pulumi.String(vars.TeamId),
+				Volume: zalandov1.PostgresqlSpecVolumeArgs{
+					Size: pulumi.String(locals.PostgresKubernetes.Spec.Container.DiskSize),
+				},
+			},
+		}, pulumi.Parent(createdNamespace))
+	if err != nil {
+		return errors.Wrap(err, "failed to create postgresql")
 	}
 
-	//if ingress is enabled, create load-balancer ingress resources
-	if locals.PostgresKubernetes.Spec.Ingress != nil &&
-		locals.PostgresKubernetes.Spec.Ingress.IsEnabled &&
-		locals.PostgresKubernetes.Spec.Ingress.EndpointDomainName != "" {
-		if err := loadBalancerIngress(ctx, locals, createdNamespace); err != nil {
-			return errors.Wrap(err, "failed to create load-balancer ingress resources")
-		}
+	if locals.PostgresKubernetes.Spec.Ingress == nil ||
+		!locals.PostgresKubernetes.Spec.Ingress.IsEnabled ||
+		locals.PostgresKubernetes.Spec.Ingress.EndpointDomainName == "" {
+		//if ingress is not enabled, no load-balancer resource is required. so just exit the function.
+		return nil
+	}
+
+	//create kubernetes-service of type load-balancer(external)
+	//this load-balancer can be used by postgres clients outside the kubernetes cluster.
+	_, err = kubernetescorev1.NewService(ctx,
+		"ingress-external-lb",
+		&kubernetescorev1.ServiceArgs{
+			Metadata: &kubernetesmetav1.ObjectMetaArgs{
+				Name:      pulumi.String("ingress-external-lb"),
+				Namespace: createdNamespace.Metadata.Name(),
+				Labels:    createdNamespace.Metadata.Labels(),
+				Annotations: pulumi.StringMap{
+					"external-dns.alpha.kubernetes.io/hostname": pulumi.String(locals.IngressExternalHostname),
+				},
+			},
+			Spec: &kubernetescorev1.ServiceSpecArgs{
+				Type: pulumi.String("LoadBalancer"),
+				Ports: kubernetescorev1.ServicePortArray{
+					&kubernetescorev1.ServicePortArgs{
+						Name:       pulumi.String("postgres"),
+						Protocol:   pulumi.String("TCP"),
+						Port:       pulumi.Int(5432),
+						TargetPort: pulumi.Int(5432),
+					},
+				},
+				Selector: pulumi.ToStringMap(locals.PostgresPodSectorLabels),
+			},
+		}, pulumi.Parent(createdNamespace))
+	if err != nil {
+		return errors.Wrapf(err, "failed to create external load balancer service")
+	}
+
+	//create kubernetes-service of type load-balancer(internal)
+	//this load-balancer can be used by postgres clients outside the kubernetes cluster
+	//but are running in the same vpc network ex: a google cloud function/aws fargate service deployed in the same vpc.
+	_, err = kubernetescorev1.NewService(ctx,
+		"ingress-internal-lb",
+		&kubernetescorev1.ServiceArgs{
+			Metadata: &kubernetesmetav1.ObjectMetaArgs{
+				Name:      pulumi.String("ingress-internal-lb"),
+				Namespace: createdNamespace.Metadata.Name(),
+				Labels:    createdNamespace.Metadata.Labels(),
+				Annotations: pulumi.StringMap{
+					"cloud.google.com/load-balancer-type":       pulumi.String("Internal"),
+					"external-dns.alpha.kubernetes.io/hostname": pulumi.String(locals.IngressInternalHostname),
+				},
+			},
+			Spec: &kubernetescorev1.ServiceSpecArgs{
+				Type: pulumi.String("LoadBalancer"),
+				Ports: kubernetescorev1.ServicePortArray{
+					&kubernetescorev1.ServicePortArgs{
+						Name:       pulumi.String("postgres"),
+						Protocol:   pulumi.String("TCP"),
+						Port:       pulumi.Int(5432),
+						TargetPort: pulumi.Int(5432),
+					},
+				},
+				Selector: pulumi.ToStringMap(locals.PostgresPodSectorLabels),
+			},
+		}, pulumi.Parent(createdNamespace))
+	if err != nil {
+		return errors.Wrapf(err, "failed to create external load balancer service")
 	}
 
 	return nil
